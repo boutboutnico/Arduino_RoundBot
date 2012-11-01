6aff7ea505b6bd7ca573a21a1048151a822cef3b
Nicolas BOUTIN <boutwork@gmail.com>
2012-11-01 11:03:14 +0100

    Kp, ki, kd find and tuned.

--- RoundBot/RoundBot.ino
+++ RoundBot/RoundBot.ino
@@ -33,19 +33,15 @@ uint32 time_limit = 40;
 
 // min = 5 (less no tested)
 // max = 60 (pwm = 255)
-float l_consigne = 0; //cm/sec
-float r_consigne = 0; //cm/sec
+float l_consigne = 50; //cm/sec
+float r_consigne = 50; //cm/sec
 
-// Result from Ziegler & Nichols method
-//float ku = 4.1;
-//float tu = 0.1;
-
-float kp = 3;
-float ki = 1;
-float kd = 0;
+float kp = 3.75;
+float ki = 0.75;
+float kd = 0.025;
 
 PID l_pid(l_consigne, st_vel.l_vel, l_cmd, kp, ki, kd);
-PID r_pid(r_consigne, st_vel.r_vel, r_cmd, 2, 3, 2);
+PID r_pid(r_consigne, st_vel.r_vel, r_cmd, kp, ki, kd);
 
 //--------------------------------------------------
 //	Setup
@@ -78,11 +74,11 @@ void setup()
 	//--------------------------------------------------
 	// PID
 	//--------------------------------------------------
-	l_pid.SetMode(AUTOMATIC);
-	l_pid.SetOutputLimits(0, 255);
-
-	r_pid.SetMode(AUTOMATIC);
-	r_pid.SetOutputLimits(0, 255);
+//	l_pid.SetMode(AUTOMATIC);
+//	l_pid.SetOutputLimits(0, 255);
+//
+//	r_pid.SetMode(AUTOMATIC);
+//	r_pid.SetOutputLimits(0, 255);
 
 	//--------------------------------------------------
 	// LCD
@@ -100,6 +96,7 @@ void setup()
 	//--------------------------------------------------
 	// Debug
 	//--------------------------------------------------
+	Serial.println("time\tcons\tvel\tcmd");
 }
 
 //--------------------------------------------------
@@ -116,40 +113,60 @@ void loop()
 	analogWrite(st_l_mot.cmd_pin, (int) l_cmd);
 	analogWrite(st_r_mot.cmd_pin, (int) r_cmd);
 
-	static uint32 motorTime = 5000;
-	if(millis() > motorTime)
-	{
-		motorTime += 5000;
+//	static uint32 motorTime = 5000;
+//	if(millis() > motorTime)
+//	{
+//		motorTime += 5000;
+//
+//		static uint8_t step = 0;
+//		switch(step++){
+//		case 0:
+//			l_consigne = 50;
+//			break;
+//		case 1:
+//			l_consigne = 0;
+//			break;
+//		case 3:
+//			l_consigne = 10;
+//			break;
+//		case 4:
+//			l_consigne = 0;
+////			step = 0;
+//			break;
+//		}
+//	}
 
-		static uint8_t step = 0;
-		switch(step++){
-		case 0:
-			l_consigne = 50;
-			break;
-		case 1:
-			l_consigne = 0;
-			break;
-		case 3:
-			l_consigne = 10;
-			break;
-		case 4:
-			l_consigne = 0;
-//			step = 0;
-			break;
-		}
-	}
-
-	//send-receive with processing if it's time
+	//--------------------------------------------------
+	//	Print Serial
+	//--------------------------------------------------
 	static uint32 serialTime = 0;
-	if(millis() > serialTime)
+//	if(millis() > serialTime)
+//	{
+//		l_pid.SerialReceive();
+//		l_pid.SerialSend();
+//		serialTime += 500;
+//	}
+
+	if(serialTime < 1500)
 	{
-		SerialReceive();
-		SerialSend();
-		serialTime += 500;
+		Serial.print(serialTime);
+		Serial.print("\t");
+		Serial.print(l_consigne);
+		Serial.print("\t");
+		Serial.print(st_vel.l_vel);
+		Serial.print("\t");
+		Serial.println(l_cmd);
+
+		serialTime += 50;
+	}
+	else
+	{
+		l_consigne = 0;
+		r_consigne = 0;
 	}
 
 	//--------------------------------------------------
-	//	Print LCD & serial
+	//	Print LCD
 	//--------------------------------------------------
 	static uint32 lcdTime = 0;
 	if(millis() > lcdTime)
@@ -200,84 +217,7 @@ void doLeftEncoder()
 //--------------------------------------------------
 //	debug
 //--------------------------------------------------
-union
-{                // This Data structure lets
-	byte asBytes[24];    // us take the byte array
-	float asFloat[6];    // sent from processing and
-}                      // easily convert it to a
-foo;                   // float array
 
-void SerialReceive()
-{
-
-	// read the bytes sent from Processing
-	int index = 0;
-	byte Auto_Man = -1;
-	byte Direct_Reverse = -1;
-	while(Serial.available() && index < 26)
-	{
-		if(index == 0) Auto_Man = Serial.read();
-		else if(index == 1) Direct_Reverse = Serial.read();
-		else foo.asBytes[index - 2] = Serial.read();
-		index++;
-	}
-
-	// if the information we got was in the correct format,
-	// read it into the system
-	if(index == 26 && (Auto_Man == 0 || Auto_Man == 1) && (Direct_Reverse == 0 || Direct_Reverse == 1))
-	{
-		l_consigne = double(foo.asFloat[0]);
-		//Input=double(foo.asFloat[1]);       // * the user has the ability to send the
-		//   value of "Input"  in most cases (as
-		//   in this one) this is not needed.
-		if(Auto_Man == 0)              // * only change the output if we are in
-		{                      //   manual mode.  otherwise we'll get an
-			l_cmd = double(foo.asFloat[2]); //   output blip, then the controller will
-		}                                     //   overwrite.
-
-		double p, i, d;              // * read in and set the controller tunings
-		p = double(foo.asFloat[3]);           //
-		i = double(foo.asFloat[4]);           //
-		d = double(foo.asFloat[5]);           //
-		l_pid.SetTunings(p, i, d);            //
-
-		if(Auto_Man == 0) l_pid.SetMode(MANUAL);          // * set the controller mode
-		else l_pid.SetMode(AUTOMATIC);             //
-
-//		if(Direct_Reverse == 0) myPID.SetControllerDirection(DIRECT); // * set the controller Direction
-//		else myPID.SetControllerDirection(REVERSE);          //
-	}
-	Serial.flush();            // * clear any random data from the serial buffer
-}
-
-// unlike our tiny microprocessor, the processing ap
-// has no problem converting strings into floats, so
-// we can just send strings.  much easier than getting
-// floats from processing to here no?
-void SerialSend()
-{
-	Serial.print("PID ");
-	Serial.print(l_consigne);
-	Serial.print(" ");
-	Serial.print(st_vel.l_vel);
-	Serial.print(" ");
-	Serial.print(l_cmd);
-	Serial.print(" ");
-
-	Serial.print(l_pid.GetKp());
-	Serial.print(" ");
-	Serial.print(l_pid.GetKi());
-	Serial.print(" ");
-	Serial.print(l_pid.GetKd());
-	Serial.print(" ");
-
-	if(l_pid.GetMode() == AUTOMATIC) Serial.print("Automatic");
-	else Serial.print("Manual");
-	Serial.print(" ");
-
-	if(l_pid.GetDirection() == DIRECT) Serial.println("Direct");
-	else Serial.println("Reverse");
-}
 /*****************************************************************************
  * END OF FILE
  *****************************************************************************/
